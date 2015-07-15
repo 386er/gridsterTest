d39228ddf665e5787ca6b1b3a7938cf6ee95205b
386er <t.burri@gmx.de>
2015-07-15 16:35:48 +0200

    Some Refactoring

--- src/modules/gridsterCode.js
+++ src/modules/gridsterCode.js
@@ -1,39 +1,55 @@
  var gridster;
 	
 	var widgets1 = [
-					['<div>0</div>', 8, 4]
+					['<div></div>', 8, 4]
 			];
 		
 	var widgets2 = [
-					['<div>0</div>', 8, 2],
-			['<div>1</div>', 8, 2]
+					['<div></div>', 8, 2],
+					['<div></div>', 8, 2]
 			];
 		
 	var widgets3 = [
-					['<div>0</div>', 4, 2],
-			['<div>1</div>', 4, 2],
-			['<div>2</div>', 8, 2]
+					['<div></div>', 4, 2],
+					['<div></div>', 4, 2],
+					['<div></div>', 8, 2]
 			];
 		
-		var widgets4 = [
-					['<div>0</div>', 4, 3],
-					['<div>1</div>', 4, 3],
-					['<div>2</div>', 4, 3],
-					['<div>3</div>', 4, 3],
+	var widgets4 = [
+					['<div></div>', 4, 3],
+					['<div></div>', 4, 3],
+					['<div></div>', 4, 3],
+					['<div></div>', 4, 3],
 			];
 
 
+			
+var currentIndex = 1;			
+			
 var gridsterElement = '<div class="gridster placeholder-box" style="left:10px">' +
-				'<ul></ul>' +
+				'<ul class="' + currentIndex +'"></ul>' +
 				'<a class="adder">+</a>' +
 				'</div>';
-		
-		
+	
+
+var getGridsterElement = function() {
+	
+	var gridsterElement = '<div class="gridster placeholder-box" style="left:10px">' +
+				'<ul class="' + currentIndex +'"></ul>' +
+				'<a class="adder">+</a>' +
+				'</div>';
+	
+	
+	return gridsterElement;
+	
+} 
+
+	
 
 		$(function(){
 
 			gridster = $(".gridster > ul").gridster({
-					widget_margins: [5, 5],
+					widget_margins: [5, 6],
 					widget_base_dimensions: [100, 55],
 			resize: {
 						enabled: true,
@@ -44,17 +60,32 @@ var gridsterElement = '<div class="gridster placeholder-box" style="left:10px">'
 
 		var widgets = widgets4;
 		
+		var initGridster = function(index) {
+			gridster = $(".gridster > ul." + index).gridster({
+				widget_margins: [5, 6],
+				widget_base_dimensions: [100, 55],
+				resize: {
+							enabled: true,
+							max_size: [4, 4],
+							min_size: [2, 2]
+						}
+				}).data('gridster');
+		};	
+	
+		initGridster(currentIndex);
 
 		var bindAdder = function() {
+			debugger;
+						currentIndex++;
+						initGridster(currentIndex);
 						$.each(widgets, function(i, widget){
 								gridster.add_widget.apply(gridster, widget)  
 						});
 
 						$('.gridster').removeClass('placeholder-box')
 						$(this).remove();
-						$('.wrapper').append(gridsterElement);
-						$('.adder').click(bindAdder);
-				
+						$('.wrapper').append(getGridsterElement());
+						$('.adder').click(bindAdder);	
 			};  
 
 
