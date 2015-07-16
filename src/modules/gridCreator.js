9ca2d19bac82b33f818e9b94670a669df9a7421e
386er <t.burri@gmx.de>
2015-07-16 15:39:09 +0200

    Introduce Mustache & Refactoring

---
+++ src/modules/gridCreator.js
@@ -0,0 +1,78 @@
+var 
+	gridster,
+	widgets1,
+	widgets2,
+	widgets3,
+	widgets4,
+	gridConfig,
+	gridTemplate;
+
+gridConfig = {
+				widget_margins: [5, 6],
+				widget_base_dimensions: [100, 55],
+				autogenerate_stylesheet: true,
+				resize: {
+					enabled: true,
+					max_size: [4, 4],
+					min_size: [1, 1]
+					}
+				}	
+		
+		
+widgets4 = [
+			['<div></div>', 4, 2],
+			['<div></div>', 4, 2],
+			['<div></div>', 4, 2],
+			['<div></div>', 4, 2],
+		];
+			
+			
+		
+gridTemplate =  '<div class="gridster placeholder-box" style="left:10px">' +
+				'<ul class="{{currentElement}}"></ul>' +
+				'</div>';
+		
+var data = {'currentElement' : 0};			
+				
+var widgets = widgets4;
+
+var bindGridsterToElement = function(index) {
+	gridster = $(".gridster > ul." + index).gridster(gridConfig).data('gridster');
+};	
+
+var html = Mustache.to_html(gridTemplate, data)
+
+$('.sub-wrapper').append(html);
+$('.gridster').removeClass('placeholder-box');
+
+
+$('.addBlock').click(function() {
+
+	data.currentElement++;
+	var html = Mustache.to_html(gridTemplate, data)
+	$('.sub-wrapper').append(html);
+	bindGridsterToElement(data.currentElement);
+	$('.gridster').removeClass('placeholder-box');
+	$.each(widgets, function(i, widget){
+						gridster.add_widget.apply(gridster, widget)  
+				});
+	
+});
+
+			
+/* widgets1 = [
+			['<div></div>', 8, 4]
+		];
+		
+widgets2 = [
+			['<div></div>', 8, 2],
+			['<div></div>', 8, 2]
+		];
+		
+widgets3 = [
+			['<div></div>', 4, 2],
+			['<div></div>', 4, 2],
+			['<div></div>', 8, 2]
+		]; */				
+				
+				
