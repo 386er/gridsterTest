4a9fd4d872297d8e19a1eaaf387f2f436abd6103
386er <t.burri@gmx.de>
2015-07-19 15:59:54 +0200

    Customize number of Elements

--- src/modules/gridCreator.js
+++ src/modules/gridCreator.js
@@ -5,7 +5,9 @@ var
 	widgets3,
 	widgets4,
 	gridConfig,
+	selecterTemplate,
 	gridTemplate;
+
 
 gridConfig = {
 				widget_margins: [5, 6],
@@ -17,22 +19,58 @@ gridConfig = {
 					min_size: [1, 1]
 					}
 				}	
+
+var widgets = {
+
+1 : [
+			['<div></div>', 6, 4]
+		],
 		
+2:  [
+			['<div></div>', 6, 2],
+			['<div></div>', 6, 2]
+		],
 		
-widgets4 = [
+3: [
+			['<div></div>', 3, 2],
+			['<div></div>', 3, 2],
+			['<div></div>', 6, 2]
+		], 
+	
+		
+4:  [
 			['<div></div>', 3, 2],
 			['<div></div>', 3, 2],
 			['<div></div>', 3, 2],
 			['<div></div>', 3, 2],
-		];
+		],
+
+5:  [
+			['<div></div>', 3, 1],
+			['<div></div>', 1, 3],
+			['<div></div>', 1, 3],
+			['<div></div>', 1, 3],
+			['<div></div>', 3, 2],
+		]		
+
+}
+
+
+selecterTemplate = '<div class="placeholder-box" style="left:10px">' +
+					'<a class="selector-box" data-key="1">1</a>' +
+					'<a class="selector-box" data-key="2">2</a>' +
+					'<a class="selector-box" data-key="3">3</a>' +
+					'<a class="selector-box" data-key="4">4</a>' +
+					'<a class="selector-box" data-key="5">5</a>' +
+					'</div>'; 
+
 					
-gridTemplate =  '<div class="gridster placeholder-box" style="left:10px">' +
-				'<ul class="{{currentElement}}"></ul>' +
-				'</div>';
+gridTemplate =  
+				'<ul class="{{currentElement}}"></ul>';
 		
 var data = {'currentElement' : 0};			
 				
-var widgets = widgets4;
+/*var widgets = widgets4;*/
 
 var bindGridsterToElement = function(index) {
 	gridster = $(".gridster > ul." + index).gridster(gridConfig).data('gridster');
@@ -41,31 +79,36 @@ var bindGridsterToElement = function(index) {
 
 $('.addBlock').click(function() {
 
-	data.currentElement++;
-	var html = Mustache.to_html(gridTemplate, data)
+	var html = Mustache.to_html(selecterTemplate)
 	$('.sub-wrapper').append(html);
-	bindGridsterToElement(data.currentElement);
-	$('.gridster').removeClass('placeholder-box');
-	$.each(widgets, function(i, widget){
-						gridster.add_widget.apply(gridster, widget)  
-				});
+	bindBox();
+	//bindGridsterToElement(data.currentElement);
+/*	$('.gridster').removeClass('placeholder-box');*/
+	//$.each(widgets, function(i, widget){
+	//					gridster.add_widget.apply(gridster, widget)  
+	//			});
 	
 });
 
+var bindBox = function() { 
+	$('.selector-box').click( function() {
+
+	data.currentElement++;
+	var html = Mustache.to_html(gridTemplate, data)
+	var numOfElements = parseInt(this.dataset.key);
+	var parent = $(this.parentElement)
+	parent.removeClass('placeholder-box');
+	parent.addClass('gridster')
+	parent.html(html)
+	$('.sub-wrapper').append(html);
+	bindGridsterToElement(data.currentElement);
+
+	$.each(widgets[numOfElements], function(i, widget){
+						gridster.add_widget.apply(gridster, widget)  
+	});
+	
+});
+
+};
+
 			
-/* widgets1 = [
-			['<div></div>', 8, 4]
-		];
-		
-widgets2 = [
-			['<div></div>', 8, 2],
-			['<div></div>', 8, 2]
-		];
-		
-widgets3 = [
-			['<div></div>', 4, 2],
-			['<div></div>', 4, 2],
-			['<div></div>', 8, 2]
-		]; */				
-				
-				
